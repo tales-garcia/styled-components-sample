e280ecaa99de07f7c4b3c40b1e74b48522635642
Tales Garcia <garcia.tales@gmail.com>
2020-12-15 19:05:29 -0300

    feat(global style): create global style and apply it to the app

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        outline: 0;
+    }
+    body {
+        background: #cfcfcf;
+        color: #4a4a4a;
+        -webkit-font-smoothing: antialiased;
+    }
+    body, input, button {
+        font-family: 'Roboto Slab', serif;
+        font-size: 16px;
+        color: #242424;
+    }
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 500;
+    }
+    button {
+        cursor: pointer;
+    }
+`;
